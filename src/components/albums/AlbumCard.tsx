6c779adccdb16eca944f0c611e7e3b03aaff6515
Aldimir <aldimirprincipal@gmail.com>
2021-03-18 13:53:50 +0100

    Upgraded styles

--- src/components/albums/AlbumCard.tsx
+++ src/components/albums/AlbumCard.tsx
@@ -1,5 +1,5 @@
 import styles from './albumCard.module.scss'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export interface IAlbum {
   title: string,
@@ -32,32 +32,63 @@ interface AlbumCardProps {
 
 const AlbumCard = (props: AlbumCardProps) => {
   const [photos, setPhotos] = useState(props.album.photos)
+  const title = props.album.title
+  const coverPhoto = props.album.photos[0]
+  const [albumCoverPhotos, setAlbumCoverPhotos] = useState<IAlbumPhoto[]>([])
+  const [secondaryPhotos, setSecondaryPhotos] = useState<IAlbumPhoto[]>([])
+  const [emptyCoverSpaces, setEmptyCoverSpaces] = useState<unknown[]>([])
+  const [emptySecondarySpaces, setEmptySecondarySpaces] = useState<unknown[]>([])
+
+  // set the three main photos of the album and remove them from the rest of the array
+  useEffect(() => {
+    let mainPhotos = []
+    let otherPhotos = []
+
+    if (photos.length >= 2) {
+      if (photos.length >= 3) {
+        mainPhotos = [photos[1], photos[2]]
+        otherPhotos = photos.slice(3)
+
+      } else {
+        mainPhotos = [photos[1]]
+        otherPhotos = photos.slice(2)
+      }
+
+      setAlbumCoverPhotos(mainPhotos)
+      setSecondaryPhotos(otherPhotos)
+    }
+    const gridSize = 9
+    const coverGridSize = 2
+    const emptySecondaryItems = Array.from({ length: gridSize - otherPhotos.length })
+    const emptyCoverItems = Array.from({ length: coverGridSize - mainPhotos.length })
+
+    setEmptyCoverSpaces(emptyCoverItems)
+    setEmptySecondarySpaces(emptySecondaryItems)
+  }, [])
+
+  console.log('render')
+  const renderItem = (photo: IAlbumPhoto, index: number) => (<img className={ index === 0 ? `${styles.photo} ${styles.roundedBottomLeft}` : `${styles.photo}` } src={photo.localUri} key={Math.random() * 100000} />)
+  const renderEmptyItem = (_:any, index: number) => (<div key={index} className={`${styles.emptyItem}`}></div>)
 
   return (
-    <div className={`${styles.card}`}>
-      <div className={`${styles.albumCover}`}>
-        <div className={`${styles.primaryCoverPhoto}`}>
-          big chungus
+    <div>
+      <div className={`${styles.card}`}>
+        <div className={`${styles.albumCover}`}>
+          <img className={`${styles.primaryCoverPhoto}`} src={coverPhoto.localUri} />
+
+          <div className={`${styles.secondaryCoverPhotos}`}>
+            { albumCoverPhotos.map(renderItem) }
+            { emptyCoverSpaces.map(renderEmptyItem) }
+          </div>
         </div>
 
-        <div className={`${styles.secondaryCoverPhotos}`}>
-          <div className={`${styles.smallPhoto} ${styles.roundedBottomLeft} mr-1`}>
-            small chungus
-          </div>
-
-          <div className={`${styles.smallPhoto}`}>
-            small chungus
-          </div>
+        <div className={`${styles.photosList}`}>
+          { secondaryPhotos.map(renderItem) }
+          { emptySecondarySpaces.map(renderEmptyItem) }
         </div>
       </div>
 
-      <div className={`${styles.photosList}`}>
-        {
-          photos.map(photo => (
-            <img className={`${styles.photo}`} src={photo.localUri} />
-          ))
-        }
-      </div>
+      <span className={styles.albumTitle}>{title} <span className={styles.albumSubtitle}>{photos.length} photos</span></span>
     </div>
   )
 }
