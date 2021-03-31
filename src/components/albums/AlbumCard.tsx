2702b32663a3e9237672809122f8cb9452cadfd4
Aldimir <aldimirprincipal@gmail.com>
2021-03-31 16:26:01 +0200

    Album photos filtering finished

--- src/components/albums/AlbumCard.tsx
+++ src/components/albums/AlbumCard.tsx
@@ -11,7 +11,7 @@ interface AlbumCardProps {
 }
 
 const AlbumCard = (props: AlbumCardProps) => {
-  const [photos, setPhotos] = useState(props.album.photos)
+  const [albumPhotos, setAlbumPhotos] = useState(props.album.photos)
   const title = props.album.name
   const [coverPhoto, setCoverPhoto] = useState<any>()
   const [coverPhotos, setCoverPhotos] = useState<IRenderablePreview[]>([])
@@ -21,32 +21,38 @@ const AlbumCard = (props: AlbumCardProps) => {
 
   // set the three main photos of the album and remove them from the rest of the array
   useEffect(() => {
-    console.log('album =>', props.album)
     getAllValues('photos', props.database).then((previews: IStoredPreview[]) => {
-      const cover = previews.find(preview => preview.originalPhotoId === photos[0].id)
-      const renderableCover = { ...cover, src: URL.createObjectURL(cover?.blob) }
-      setCoverPhoto(renderableCover)
+      // Set the first selected picture as the cover photo of the album and transform it from a blob
+      const albumCoverPhoto = previews.find(preview => preview.originalPhotoId === albumPhotos[0].id)
+      const renderableCover = { ...albumCoverPhoto, src: URL.createObjectURL(albumCoverPhoto?.blob) }
+
       let mainPhotos: any[] = []
       let otherPhotos: IStoredPreview[] = []
       let mainRenderablePreviews: IRenderablePreview[] = []
       let secondaryRenderablePreviews: IRenderablePreview[] = []
 
-      if (photos.length >= 2) {
-        if (photos.length >= 3) {
+      if (albumPhotos.length >= 2) {
+        if (albumPhotos.length >= 3) {
           // The three main photos
-          mainPhotos = previews.filter(preview => preview.originalPhotoId === photos[1].id || preview.originalPhotoId === photos[2].id)
-          // The rest of the array
-          otherPhotos = previews.filter(preview => preview.originalPhotoId !== photos[1].id || preview.originalPhotoId !== photos[2].id)
+          mainPhotos = previews.filter(preview => preview.originalPhotoId === albumPhotos[1].id || preview.originalPhotoId === albumPhotos[2].id)
+          // The rest of the photos
+          for (let i = 0; i < previews.length; i++) {
+            for (let j = 0; j < albumPhotos.length; j++) {
+              if (previews[i].originalPhotoId === albumPhotos[j].id) {
+                const preview = previews[i]
+
+                otherPhotos.push(preview)
+              }
+            }
+          }
 
           // Transforming the blobs to render the filtered previews
           mainRenderablePreviews = mainPhotos.map(photo => ({ ...photo, src: URL.createObjectURL(photo.blob) }))
           secondaryRenderablePreviews = otherPhotos.map(photo => ({ ...photo, src: URL.createObjectURL(photo.blob) }))
         } else {
-          mainPhotos = [previews.find(preview => preview.originalPhotoId === photos[1].id)]
-          otherPhotos = previews.filter(preview => preview.originalPhotoId !== photos[1].id)
+          mainPhotos = [previews.find(preview => preview.originalPhotoId === albumPhotos[1].id)]
 
           mainRenderablePreviews = mainPhotos.map(photo => ({ ...photo, src: URL.createObjectURL(photo.blob) }))
-          secondaryRenderablePreviews = otherPhotos.map(photo => ({ ...photo, src: URL.createObjectURL(photo.blob) }))
         }
 
         setCoverPhotos(mainRenderablePreviews)
@@ -57,6 +63,7 @@ const AlbumCard = (props: AlbumCardProps) => {
       const emptySecondaryItems = Array.from({ length: gridSize - otherPhotos.length })
       const emptyCoverItems = Array.from({ length: coverGridSize - mainPhotos.length })
 
+      setCoverPhoto(renderableCover)
       setEmptyCoverSpaces(emptyCoverItems)
       setEmptySecondarySpaces(emptySecondaryItems)
     })
@@ -64,6 +71,7 @@ const AlbumCard = (props: AlbumCardProps) => {
 
   const renderItem = (photo: IRenderablePreview, index: number) => <img className={index === 0 ? `${styles.photo} ${styles.roundedBottomLeft}` : `${styles.photo}`} src={photo.src} key={photo.previewId} />
   const renderEmptyItem = (_: any, index: number) => <div key={index} className={`${styles.emptyItem}`}></div>
+  const previewNotYetDownloaded = () => <span className={`${styles.photo}`}>Preview yet not downloaded</span>
 
   return (
     <div className={`${styles.mainContainer}`}>
@@ -83,7 +91,7 @@ const AlbumCard = (props: AlbumCardProps) => {
         </div>
       </div>
 
-      <span className={styles.albumTitle}>{title} <span className={styles.albumSubtitle}>{photos.length} photos</span></span>
+      <span className={styles.albumTitle}>{title} <span className={styles.albumSubtitle}>{albumPhotos.length} photos</span></span>
     </div>
   )
 }
