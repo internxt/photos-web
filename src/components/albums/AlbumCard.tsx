ccf83353ea18c4ff6a960f85dd890993e8594153
Aldimir <aldimirprincipal@gmail.com>
2021-04-01 08:38:37 +0200

    refractored album photos filtering

--- src/components/albums/AlbumCard.tsx
+++ src/components/albums/AlbumCard.tsx
@@ -1,9 +1,9 @@
 import styles from './albumCard.module.scss'
-import { useEffect, useState } from 'react';
-import { IDBPDatabase } from 'idb';
-import { getAllValues, getValue } from '../../lib/utils/indexedDB';
-import { IRenderablePreview, IStoredPreview } from '../../lib/types/photos';
-import { IAlbum } from '../../lib/types/albums';
+import { useEffect, useState } from 'react'
+import { IDBPDatabase } from 'idb'
+import { getAllValues } from '../../lib/utils/indexedDB'
+import { IRenderablePreview, IStoredPreview } from '../../lib/types/photos'
+import { IAlbum } from '../../lib/types/albums'
 
 interface AlbumCardProps {
   album: IAlbum,
@@ -19,51 +19,42 @@ const AlbumCard = (props: AlbumCardProps) => {
   const [emptyCoverSpaces, setEmptyCoverSpaces] = useState<any[]>([])
   const [emptySecondarySpaces, setEmptySecondarySpaces] = useState<any[]>([])
 
-  // set the three main photos of the album and remove them from the rest of the array
   useEffect(() => {
-    getAllValues('photos', props.database).then((previews: IStoredPreview[]) => {
-      // Set the first selected picture as the cover photo of the album and transform it from a blob
+    getAllValues("photos", props.database).then((previews: IStoredPreview[]) => {
+      // Set the first picture the user selected as the cover photo of the album and transform it from a blob
       const albumCoverPhoto = previews.find(preview => preview.originalPhotoId === albumPhotos[0].id)
       const renderableCover = { ...albumCoverPhoto, src: URL.createObjectURL(albumCoverPhoto?.blob) }
 
-      let mainPhotos: any[] = []
+      let mainPhotos: IStoredPreview[] = []
       let otherPhotos: IStoredPreview[] = []
       let mainRenderablePreviews: IRenderablePreview[] = []
       let secondaryRenderablePreviews: IRenderablePreview[] = []
 
-      if (albumPhotos.length >= 2) {
-        if (albumPhotos.length >= 3) {
-          // The three main photos
-          mainPhotos = previews.filter(preview => preview.originalPhotoId === albumPhotos[1].id || preview.originalPhotoId === albumPhotos[2].id)
-          // The rest of the photos
-          for (let i = 0; i < previews.length; i++) {
-            for (let j = 0; j < albumPhotos.length; j++) {
-              if (previews[i].originalPhotoId === albumPhotos[j].id) {
-                const preview = previews[i]
+      if (albumPhotos.length >= 3) {
+        const lookup = albumPhotos.reduce((acc, cur) => ({ ...acc, [cur.id]: cur }), {})
+        console.log('lookup =>', lookup)
+        // The two main photos
+        mainPhotos = previews.filter(preview => preview.originalPhotoId === albumPhotos[1].id || preview.originalPhotoId === albumPhotos[2].id)
+        // The rest of the photos
+        otherPhotos = previews.filter(photo => lookup.hasOwnProperty(photo.originalPhotoId))
 
-                otherPhotos.push(preview)
-              }
-            }
-          }
+        // Transforming the blobs to render the filtered previews
+        mainRenderablePreviews = mainPhotos.map(photo => ({ ...photo, src: URL.createObjectURL(photo.blob) }))
+        secondaryRenderablePreviews = otherPhotos.map(photo => ({ ...photo, src: URL.createObjectURL(photo.blob) }))
 
-          // Transforming the blobs to render the filtered previews
-          mainRenderablePreviews = mainPhotos.map(photo => ({ ...photo, src: URL.createObjectURL(photo.blob) }))
-          secondaryRenderablePreviews = otherPhotos.map(photo => ({ ...photo, src: URL.createObjectURL(photo.blob) }))
-        } else {
-          mainPhotos = [previews.find(preview => preview.originalPhotoId === albumPhotos[1].id)]
-
-          mainRenderablePreviews = mainPhotos.map(photo => ({ ...photo, src: URL.createObjectURL(photo.blob) }))
-        }
-
-        setCoverPhotos(mainRenderablePreviews)
-        setSecondaryPhotos(secondaryRenderablePreviews)
+      } else if (albumPhotos.length >= 2) {
+        mainPhotos = previews.filter(preview => preview.originalPhotoId === albumPhotos[1].id)
+        mainRenderablePreviews = mainPhotos.map(photo => ({ ...photo, src: URL.createObjectURL(photo.blob) }))
       }
+
       const gridSize = 9
       const coverGridSize = 2
       const emptySecondaryItems = Array.from({ length: gridSize - otherPhotos.length })
       const emptyCoverItems = Array.from({ length: coverGridSize - mainPhotos.length })
 
       setCoverPhoto(renderableCover)
+      setCoverPhotos(mainRenderablePreviews)
+      setSecondaryPhotos(secondaryRenderablePreviews)
       setEmptyCoverSpaces(emptyCoverItems)
       setEmptySecondarySpaces(emptySecondaryItems)
     })
@@ -96,4 +87,4 @@ const AlbumCard = (props: AlbumCardProps) => {
   )
 }
 
-export default AlbumCard;
+export default AlbumCard
